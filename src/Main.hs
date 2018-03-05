34c10e192ccd1ece42e9d26559eb26cfb926f07b
Kaptch <kaptch@gmail.com>
2018-03-06 00:28:26 +0300

    simple parsers

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,15 @@
 module Main where
+import System.Environment
 import Parser
 import Apply
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = do fname <- getArgs
+          case fname of
+              [] -> error "meh"
+              x:[] -> error "meh"
+              fname:word:[] -> putStrLn (fname ++ " " ++ word)
+                               {-
+                               scheme <- parseFromFile parseScheme fname
+                               
+                               -}
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,2 +1,38 @@
 module Parser where
-    import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec
+
+{-
+Grammar:
+    <left>->[.]<right>
+
+    ab\to a,\\
+    b\to\varepsilon,\\
+    a\to b;
+
+    ab\to a,\\
+    b\tof\varepsilon,\\
+    a\to b;
+
+Output example:
+    0 \xtod{(ж)} a0 \xtod{(г)}0a \xtod{(е)}0b\xtod{(а)}1\\
+-}
+
+eol :: GenParser Char st String
+eol = string ";" <|> string ",\\"
+
+word :: GenParser Char st String
+word = many $ noneOf "->."
+{-
+substitutionType :: GenParser Char st String
+substitutionType = try
+
+substitution :: GenParser Char st [[String]]
+substitution = do result <- many line
+                  eof
+                  return result
+
+line :: GenParser Char st [String]
+line = do left <- word
+
+          right <- word
+-}
