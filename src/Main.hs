1320d16dfdba1742ce5f92309e60891110158364
Kaptch <kaptch@gmail.com>
2018-04-15 20:35:44 +0300

    import Substitution

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,15 @@ module Main where
 import System.Environment
 import Parser
 import Apply
+import Substitution
+import Text.ParserCombinators.Parsec
 
 main :: IO ()
 main = do fname <- getArgs
           case fname of
               [] -> error "meh"
-              x:[] -> error "meh"
-              fname:word:[] -> putStrLn (fname ++ " " ++ word)
+              [x] -> error "meh"
+              [fname, word] -> print $ parseSubstitutions "dfdg->fdf\naaa->.dfdf\n"
                                {-
                                scheme <- parseFromFile parseScheme fname
-                               
                                -}
