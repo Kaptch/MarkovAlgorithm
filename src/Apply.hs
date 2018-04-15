53918403801a94968a884e42ffc5da0d4fe91897
Kaptch <kaptch@gmail.com>
2018-04-15 22:18:08 +0300

    replaceOnce

--- src/Apply.hs
+++ src/Apply.hs
@@ -1,5 +1,20 @@
 module Apply where
 import Substitution
+import Control.Monad.State
+import Data.List
 
-apply :: [Int] -> Int
-apply = undefined
+replaceOnce :: String -> String -> String -> String
+replaceOnce _ _ [] = []
+replaceOnce sb rplmt str@(x:xs) = if sb `isPrefixOf` str
+    then rplmt ++ drop (length sb) str
+    else x : replaceOnce sb rplmt xs
+
+findSubs :: [Substitution] -> String -> Substitution
+findSubs (x:xs) str = undefined
+
+apply :: [Substitution] -> StateT String IO ()
+apply l =
+    do st <- get
+       liftIO $ print st
+
+       return ()
