040947af5157535028b171d232c8e01119e3b9b9
Kaptch <kaptch@gmail.com>
2018-04-18 16:16:21 +0300

    usage

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,16 @@ import Substitution
 import Text.ParserCombinators.Parsec
 import Control.Monad.State
 
+usage :: IO ()
+usage = do
+    putStrLn "usage: <filename> <input word>"
+    putStrLn "  substitution file should consist of lines: <left>->[.]<right>"
+
 main :: IO ()
 main = do args <- getArgs
           case args of
-              [] -> error "meh"
-              [x] -> error "meh"
+              [] -> usage
+              [x] -> usage
               (fname:word:_) -> do
                 fileCont <- readFile fname
                 let trySubs = parseSubstitutions fname fileCont in
